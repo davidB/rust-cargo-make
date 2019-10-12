52eb288328dba2b8ae97a0ad62be75b9e6f75ed5
David Bernard <david.bernard.31@gmail.com>
2019-10-12 20:49:13 +0200

    :bug: use a tmp folder

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,14 @@ import * as io from '@actions/io';
 import * as tc from '@actions/tool-cache';
 import * as os from 'os';
 import * as path from 'path';
-import * as fs from 'fs';
 
 async function run() {
-  const tmpFolder = os.tmpdir()
+  const tmpFolder = path.join(os.tmpdir(), "setup-rust-cargo-make")
+  await io.mkdirP(tmpFolder)
   try {
     const cargoMakeVersion = core.getInput('version')
     console.log(`installing cargo-make ${cargoMakeVersion} ...`)
-    const platform =  core.getInput('platform') || process.platform
+    const platform =  process.env['PLATFORM'] || process.platform
     core.debug(platform)
 
     const execFolder = path.join(os.homedir(), '.cargo', 'bin')
@@ -20,19 +20,23 @@ async function run() {
     let exe_ext = ''
     let arch = 'noarch'
     let archTopFolder = ''
-    if (platform === 'win32') {
-      arch = 'x86_64-pc-windows-msvc'
-      archTopFolder = ''
-      exe_ext = '.exe'
-    } else if (platform === 'darwin') {
-      arch = 'x86_64-apple-darwin'
-      archTopFolder = arch
-    } else if (platform === 'linux') {
-      arch = 'x86_64-unknown-linux-musl'
-      archTopFolder = arch
-    } else {
-      core.setFailed('unsupported platform:' + process.platform)
-      return
+    switch(platform) {
+      case 'win32':
+        arch = 'x86_64-pc-windows-msvc'
+        archTopFolder = ''
+        exe_ext = '.exe'
+        break;
+      case 'darwin':
+        arch = 'x86_64-apple-darwin'
+        archTopFolder = `cargo-make-v${cargoMakeVersion}-${arch}`
+        break;
+      case 'linux':
+        arch = 'x86_64-unknown-linux-musl'
+        archTopFolder = `cargo-make-v${cargoMakeVersion}-${arch}`
+        break;
+      default:
+        core.setFailed('unsupported platform:' + platform)
+        return
     }
     const archive = `cargo-make-v${cargoMakeVersion}-${arch}`
     const cargoMakeArchive = await tc.downloadTool(`https://github.com/sagiegurari/cargo-make/releases/download/${cargoMakeVersion}/${archive}.zip`)
@@ -42,9 +46,11 @@ async function run() {
     await io.mv(path.join(extractedFolder, archTopFolder, exec), execPath).then(() =>
       io.rmRF(path.join(extractedFolder, archive))
     )
-    core.debug(`installed: ${execPath} : ${fs.existsSync(execPath)}`)
+    core.debug(`installed: ${execPath}`)
   } catch (error) {
     core.setFailed(error.message)
+  } finally {
+    io.rmRF(tmpFolder)
   }
 }
 
