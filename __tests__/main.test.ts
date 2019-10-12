983dcd0d97fe127baa787132403b7f84723c3343
David Bernard <david.bernard.31@gmail.com>
2019-10-12 18:47:03 +0200

    :bug: the archive contains a folder, extract the executable from the folder

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,10 +1,12 @@
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
+import * as os from 'os'
 
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
-    process.env['INPUT_MILLISECONDS'] = '500';
+    process.env['RUNNER_TEMP'] = os.tmpdir()
+    process.env['INPUT_VERSION'] = "0.22.2"
     const ip = path.join(__dirname, '..', 'lib', 'main.js');
     const options: cp.ExecSyncOptions = {
         env: process.env
--- src/main.ts
+++ src/main.ts
@@ -2,29 +2,36 @@ import * as core from '@actions/core';
 import * as io from '@actions/io';
 import * as tc from '@actions/tool-cache';
 import * as os from 'os';
+import { CodeGenerator } from '@babel/generator';
 
 async function run() {
+  const tmpFolder = os.tmpdir()
   try {
     const cargoMakeVersion = core.getInput('version')
     console.log(`installing cargo-make ${cargoMakeVersion} ...`)
 
-    const execFolder = os.homedir + '/.cargo/bin2'
+    const execFolder = os.homedir + '/.cargo/bin'
     core.debug(execFolder)
     await io.mkdirP(execFolder)
+    core.debug(process.platform)
     if (process.platform === 'win32') {
-      const cargoMakeArchive = await tc.downloadTool(`https://github.com/sagiegurari/cargo-make/releases/download/${cargoMakeVersion}/cargo-make-v${cargoMakeVersion}-x86_64-pc-windows-msvc.zip`)
-      const extractedFolder = await tc.extractZip(cargoMakeArchive, execFolder)
-      core.setOutput('installed at ', extractedFolder);
+      const archive = `cargo-make-v${cargoMakeVersion}-x86_64-pc-windows-msvc`
+      const cargoMakeArchive = await tc.downloadTool(`https://github.com/sagiegurari/cargo-make/releases/download/${cargoMakeVersion}/${archive}.zip`)
+      const extractedFolder = await tc.extractZip(cargoMakeArchive, tmpFolder)
+      io.mv(`${extractedFolder}/${archive}/cargo-make.exe`, execFolder)
+      io.rmRF(`${extractedFolder}/${archive}`)
+      core.setOutput('installed', execFolder)
       // const node12Path = tc.downloadTool('https://nodejs.org/dist/v12.7.0/node-v12.7.0-win-x64.7z')
       // const node12ExtractedFolder = await tc.extract7z(node12Path, 'path/to/extract/to')
     } else if (process.platform === 'darwin') {
-      const cargoMakeArchive = await tc.downloadTool(`https://github.com/sagiegurari/cargo-make/releases/download/${cargoMakeVersion}/cargo-make-v${cargoMakeVersion}-x86_64-apple-darwin.zip`)
-      const extractedFolder = await tc.extractZip(cargoMakeArchive, execFolder)
-      core.setOutput('installed at ', extractedFolder)
+      const archive = `cargo-make-v${cargoMakeVersion}-x86_64-apple-darwin`
     } else if (process.platform === 'linux') {
-      const cargoMakeArchive = await tc.downloadTool(`https://github.com/sagiegurari/cargo-make/releases/download/${cargoMakeVersion}/cargo-make-v${cargoMakeVersion}-x86_64-unknown-linux-musl.zip`)
-      const extractedFolder = await tc.extractZip(cargoMakeArchive, execFolder)
-      core.setOutput('installed at ', extractedFolder)
+      const archive = `cargo-make-v${cargoMakeVersion}-x86_64-unknown-linux-musl`
+      const cargoMakeArchive = await tc.downloadTool(`https://github.com/sagiegurari/cargo-make/releases/download/${cargoMakeVersion}/${archive}.zip`)
+      const extractedFolder = await tc.extractZip(cargoMakeArchive, tmpFolder)
+      io.mv(`${extractedFolder}/${archive}/cargo-make`, execFolder)
+      io.rmRF(`${extractedFolder}/${archive}`)
+      core.setOutput('installed', execFolder)
     } else {
       core.setFailed('unsupported platform:' + process.platform)
     }
