ac7f06b2678425ab611a39dd9abde9c88e5c25eb
David Bernard <david.bernard.31@gmail.com>
2020-05-24 14:49:20 +0200

    :rotating_light: format code (prettier) + fix eslint warning

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -6,30 +6,30 @@ import * as os from 'os'
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs with fixed version="0.30.7"', () => {
   process.env['RUNNER_TEMP'] = os.tmpdir()
-  process.env['INPUT_VERSION'] = "0.30.7"
-  const ip = path.join(__dirname, '..', 'lib', 'main.js');
+  process.env['INPUT_VERSION'] = '0.30.7'
+  const ip = path.join(__dirname, '..', 'lib', 'main.js')
   const options: cp.ExecSyncOptions = {
     env: process.env
-  };
-  console.log(cp.execSync(`node ${ip}`, options).toString());
-});
+  }
+  console.log(cp.execSync(`node ${ip}`, options).toString())
+})
 
 test('test runs with version="latest"', () => {
   process.env['RUNNER_TEMP'] = os.tmpdir()
-  process.env['INPUT_VERSION'] = "latest"
-  const ip = path.join(__dirname, '..', 'lib', 'main.js');
+  process.env['INPUT_VERSION'] = 'latest'
+  const ip = path.join(__dirname, '..', 'lib', 'main.js')
   const options: cp.ExecSyncOptions = {
     env: process.env
-  };
-  console.log(cp.execSync(`node ${ip}`, options).toString());
-});
+  }
+  console.log(cp.execSync(`node ${ip}`, options).toString())
+})
 
 test('test runs without version defined', () => {
   process.env['RUNNER_TEMP'] = os.tmpdir()
   // process.env['INPUT_VERSION'] = "latest"
-  const ip = path.join(__dirname, '..', 'lib', 'main.js');
+  const ip = path.join(__dirname, '..', 'lib', 'main.js')
   const options: cp.ExecSyncOptions = {
     env: process.env
-  };
-  console.log(cp.execSync(`node ${ip}`, options).toString());
-});
+  }
+  console.log(cp.execSync(`node ${ip}`, options).toString())
+})
--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,40 @@
-import * as core from '@actions/core';
-import * as io from '@actions/io';
-import * as tc from '@actions/tool-cache';
-import * as os from 'os';
-import * as path from 'path';
+import * as core from '@actions/core'
+import * as io from '@actions/io'
+import * as tc from '@actions/tool-cache'
+import * as os from 'os'
+import * as path from 'path'
 // use typed-rest-client like actions/tool-cache
-import * as httpm from 'typed-rest-client/HttpClient';
+import * as httpm from 'typed-rest-client/HttpClient'
 
-let httpc: httpm.HttpClient = new httpm.HttpClient('vsts-node-api');
+const httpc: httpm.HttpClient = new httpm.HttpClient('vsts-node-api')
 
 async function findVersionLatest(): Promise<string> {
-  console.log(`search latest version of cargo-make`)
-  const response = await httpc.get('https://api.github.com/repos/sagiegurari/cargo-make/releases/latest')
+  core.info(`search latest version of cargo-make`)
+  const response = await httpc.get(
+    'https://api.github.com/repos/sagiegurari/cargo-make/releases/latest'
+  )
   const body = await response.readBody()
   return Promise.resolve(JSON.parse(body).tag_name || '0.30.7')
 }
 
 async function findVersion(): Promise<string> {
   const inputVersion = core.getInput('version')
-  if (inputVersion === 'latest' || inputVersion == null || inputVersion == undefined) {
+  if (
+    inputVersion === 'latest' ||
+    inputVersion == null ||
+    inputVersion === undefined
+  ) {
     return findVersionLatest()
   }
   return Promise.resolve(inputVersion)
 }
 
-async function run() {
-  const tmpFolder = path.join(os.tmpdir(), "setup-rust-cargo-make")
-  const _ = await io.mkdirP(tmpFolder)
+async function run(): Promise<void> {
+  const tmpFolder = path.join(os.tmpdir(), 'setup-rust-cargo-make')
+  await io.mkdirP(tmpFolder)
   try {
     const cargoMakeVersion = await findVersion()
-    console.log(`installing cargo-make ${cargoMakeVersion} ...`)
+    core.info(`installing cargo-make ${cargoMakeVersion} ...`)
     const platform = process.env['PLATFORM'] || process.platform
     core.debug(platform)
 
@@ -36,40 +42,40 @@ async function run() {
     core.debug(execFolder)
     await io.mkdirP(execFolder)
 
-    let exe_ext = ''
+    let exeExt = ''
     let arch = 'noarch'
     let archTopFolder = ''
     switch (platform) {
       case 'win32':
         arch = 'x86_64-pc-windows-msvc'
         archTopFolder = ''
-        exe_ext = '.exe'
-        break;
+        exeExt = '.exe'
+        break
       case 'darwin':
         arch = 'x86_64-apple-darwin'
         archTopFolder = `cargo-make-v${cargoMakeVersion}-${arch}`
-        break;
+        break
       case 'linux':
         arch = 'x86_64-unknown-linux-musl'
         archTopFolder = `cargo-make-v${cargoMakeVersion}-${arch}`
-        break;
+        break
       default:
-        core.setFailed('unsupported platform:' + platform)
+        core.setFailed(`unsupported platform: ${platform}`)
         return
     }
     const archive = `cargo-make-v${cargoMakeVersion}-${arch}`
     const url = `https://github.com/sagiegurari/cargo-make/releases/download/${cargoMakeVersion}/${archive}.zip`
-    console.log(`downloading ${url}`)
+    core.info(`downloading ${url}`)
     const cargoMakeArchive = await tc.downloadTool(url)
     const extractedFolder = await tc.extractZip(cargoMakeArchive, tmpFolder)
-    const exec = `cargo-make${exe_ext}`
+    const exec = `cargo-make${exeExt}`
     const execPath = path.join(execFolder, exec)
-    await io.mv(path.join(extractedFolder, archTopFolder, exec), execPath).then(() =>
-      io.rmRF(path.join(extractedFolder, archive))
-    )
+    await io
+      .mv(path.join(extractedFolder, archTopFolder, exec), execPath)
+      .then(async () => io.rmRF(path.join(extractedFolder, archive)))
     core.debug(`installed: ${execPath}`)
   } catch (error) {
-    console.error(error)
+    core.error(error)
     core.setFailed(error.message)
   } finally {
     io.rmRF(tmpFolder)
@@ -77,6 +83,6 @@ async function run() {
 }
 
 run().then(
-  () => console.log("done"),
-  (err) => console.error(err)
-);
+  () => core.info('done'),
+  err => core.error(err)
+)
