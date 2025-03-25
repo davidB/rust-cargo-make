1d1aa6d2ee55e0fdbf5fca09731bbe1d5701bf87
David Bernard <david.bernard.31@gmail.com>
2025-03-25 10:37:28 +0100

    🚨 try to please a linter and to align with actions/typescript-action base code

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -2,7 +2,7 @@
  * Unit tests for the action's entrypoint, src/index.ts
  */
 
-import * as main from '../src/main'
+import * as main from '../src/main.js'
 
 // Mock the action's entrypoint
 const runMock = jest.spyOn(main, 'run').mockImplementation()
@@ -10,7 +10,7 @@ const runMock = jest.spyOn(main, 'run').mockImplementation()
 describe('index', () => {
   it('calls run when imported', () => {
     // eslint-disable-next-line @typescript-eslint/no-require-imports
-    require('../src/index')
+    require('../src/index.js')
 
     expect(runMock).toHaveBeenCalled()
   })
